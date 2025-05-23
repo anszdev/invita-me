70dcbd104ec820f7e74134a6a6435a014e67658b
anszdev <anszdev@outlook.com>
2025-05-23 00:28:19 -0600

    refactor(navbar): improve responsiveness and animation handling

--- src/components/landing/Navbar/NavLink.tsx
+++ src/components/landing/Navbar/NavLink.tsx
@@ -1,14 +1,20 @@
+import clsx from "clsx";
+
 type Props = {
   href: string;
   children: React.ReactNode;
   delay?: number;
+  animate?: boolean;
 };
 
-export function NavLink({ href, children, delay }: Props) {
+export function NavLink({ href, children, delay, animate }: Props) {
   return (
     <a
       href={href}
-      className="flex justify-center items-center font-black bg-light text-3xl uppercase p-6 h-32 rounded-3xl hover:bg-thin transition-colors duration-300 ease-linear hover:text-primary"
+      className={clsx(
+        "flex justify-center items-center font-black bg-light text-3xl uppercase p-4 md:p-6 h-24 md:h-32 rounded-3xl hover:bg-thin transition-colors duration-300 ease-linear hover:text-primary",
+        animate ? "animate-jump-above" : ""
+      )}
       style={{ animationDelay: `${delay}s` }}
     >
       {children}
--- src/components/landing/Navbar/Navbar.tsx
+++ src/components/landing/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ const navbarLinks = [
   { href: "#", label: "Servicios" },
   { href: "#", label: "Planes" },
   { href: "#", label: "FAQ" },
-  { href: "#", label: "Nuestros diseños" },
+  { href: "#", label: "Diseños" },
   { href: "#", label: "Contacto" },
 ];
 
@@ -25,8 +25,8 @@ export function Navbar() {
 
   return (
     <>
-      <nav className="max-w-[50rem] bg-dark mx-auto sticky w-full py-4 px-10 top-[2.5rem] h-[4.4rem] rounded-full flex items-center justify-between">
-        <a className="text-thin font-medium text-lg" href="/">
+      <nav className="max-w-[50rem] bg-dark mx-auto sticky md:w-full py-4 px-6 md:px-10 top-4 md:top-[2.5rem] h-16 md:h-[4.4rem] rounded-full flex items-center justify-between w-[95%]">
+        <a className="text-thin font-medium text-lg md:text-xl" href="/">
           🪅Invita<span className="font-light text-thin">.me</span>
         </a>
         <div className="flex items-center gap-2.5">
@@ -36,20 +36,16 @@ export function Navbar() {
       </nav>
       <div
         className={clsx(
-          "max-w-[50rem] bg-dark mx-auto top-[7.2rem] sticky rounded-[3rem] p-10 transition-opacity duration-300 flex flex-wrap gap-3.5 justify-center h-[21.88rem]",
+          "max-w-[50rem] w-[95%] md:w-full bg-dark mx-auto top-[5.3rem] md:top-[7.2rem] sticky rounded-[3rem] p-6 md:p-10 flex flex-wrap gap-2.5 md:gap-3.5 justify-center h-fit transition-opacity duration-200 ease-linear",
           isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
         )}
         onClick={handleCloseMenu}
       >
-        {isOpen && (
-          <>
-            {navbarLinks.map(({ href, label }, i) => (
-              <NavLink key={label} href={href} delay={i * 0.02}>
-                {label}
-              </NavLink>
-            ))}
-          </>
-        )}
+        {navbarLinks.map(({ href, label }, i) => (
+          <NavLink key={label} href={href} delay={i * 0.02} animate={isOpen}>
+            {label}
+          </NavLink>
+        ))}
       </div>
     </>
   );
