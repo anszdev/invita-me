2a2c9d44c1d4b165693e3e907ccb91ac3d399d3b
anszdev <anszdev@outlook.com>
2025-05-23 23:51:51 -0600

    style(NavLoginButton): update hover background color for better visibility

--- src/components/landing/Navbar/NavLoginButton.tsx
+++ src/components/landing/Navbar/NavLoginButton.tsx
@@ -3,7 +3,7 @@ import { Google } from "@components/assets/icons/Google";
 export function NavbarLoginButton() {
   return (
     <button
-      className="py-2 px-3 flex gap-2 items-center justify-center rounded-full hover:bg-[#2A263B] transition-colors duration-300 ease-linear relative group cursor-pointer"
+      className="py-2 px-3 flex gap-2 items-center justify-center rounded-full hover:bg-[#5E5A73] transition-colors duration-300 ease-linear relative group cursor-pointer"
       id="google-login"
     >
       <Google size={20} />
