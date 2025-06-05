e7677cf15182d66dbed2cb7c6a64266048bfc4ee
anszdev <anszdev@outlook.com>
2025-06-05 00:35:18 -0600

    fix: change eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
-  ...eslintPluginAstro.configs.all,
+  ...eslintPluginAstro.configs.recommended,
   {
     rules: {
       // override/add rules settings here, such as:
