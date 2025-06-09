7e436e0ae5ff0819f0a5e1ae6e0db89c41f16a59
anszdev <anszdev@outlook.com>
2025-06-09 13:58:04 -0600

    dependency: remove react and add svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +10,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [svelte()],
 });
