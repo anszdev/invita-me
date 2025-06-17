5d6294734116658481a52d1560c4f9b33f97e178
anszdev <anszdev@outlook.com>
2025-06-16 23:08:50 -0600

    Add invitation content options

--- src/utils/staticData.ts
+++ src/utils/staticData.ts
@@ -3,3 +3,14 @@ export const EVENT_CATEGORIES = [
   { event: "Xv Años", img: "xv" },
   { event: "Cumpleaños", img: "cumple" },
 ];
+
+export const CONTENT_INVITATION = [
+  "Cuenta regresiva",
+  "Galería de Fotos",
+  "Programa del evento",
+  "Frases o dedicatorias especiales",
+  "Mesa de Regalos",
+  "Ubicación del evento",
+  "Confirmar Asistencia",
+  "Código de vestimenta",
+];
